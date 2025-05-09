9df08bba0c4a406ae4e91bc56e700a713b845a3c
wa <wangao@hljzjkj.com>
2025-05-09 12:59:19 +0800

    移除AI依赖，封装新版统一认证

---
+++ src/main/java/tech/hljzj/infrastructure/controller/TokenController.java
@@ -0,0 +1,21 @@
+package tech.hljzj.infrastructure.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.hljzj.framework.bean.R;
+import tech.hljzj.framework.security.bean.TokenAuthentication;
+import tech.hljzj.framework.service.ITokenAuthenticateService;
+import tech.hljzj.framework.util.web.AuthUtil;
+
+@RestController
+public class TokenController {
+    @Autowired
+    private ITokenAuthenticateService authenticateService;
+
+    @PostMapping("/token")
+    public R<TokenAuthentication> token() throws Exception {
+        TokenAuthentication authentication = AuthUtil.getAuthentication();
+        return R.ok(authenticateService.authenticate(authentication.getToken()));
+    }
+}
--- src/main/java/tech/hljzj/infrastructure/interceptor/AppInterceptor.java
+++ src/main/java/tech/hljzj/infrastructure/interceptor/AppInterceptor.java
@@ -3,8 +3,10 @@ package tech.hljzj.infrastructure.interceptor;
 import cn.hutool.core.util.StrUtil;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import jakarta.validation.constraints.NotNull;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 import tech.hljzj.framework.bean.R;
 import tech.hljzj.framework.interceptor.BaseInterceptor;
@@ -15,9 +17,6 @@ import tech.hljzj.infrastructure.config.AppLoginUserInfo;
 import tech.hljzj.infrastructure.domain.SysApp;
 import tech.hljzj.infrastructure.service.SysAppService;
 import tech.hljzj.infrastructure.util.AppScopeHolder;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * 应用信息校验拦截
@@ -32,10 +31,12 @@ public class AppInterceptor implements BaseInterceptor {
         this.sysAppService = sysAppService;
     }
 
+    private static final AntPathRequestMatcher DEFAULT_ANT_PATH_REQUEST_MATCHER = new AntPathRequestMatcher("/token", "POST");
+
     @Override
-    public boolean preHandle(@NotNull HttpServletRequest request,
-                             @NotNull HttpServletResponse response,
-                             @NotNull Object handler) {
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) {
         String token = AppScopeHolder.getRawAppToken();
         if (StrUtil.isBlank(token)) {
             return true;
@@ -46,11 +47,15 @@ public class AppInterceptor implements BaseInterceptor {
         try {
             JWT.require(Algorithm.HMAC384(sysApp.getSecret())).build().verify(token);
             if (AuthUtil.isLogin()) {
-                LoginUser loginUser = AuthUtil.getLoginUser();
-                AppLoginUserInfo userInfo = (AppLoginUserInfo) loginUser.getUserInfo();
-                String s = userInfo.getLoginAppId();
-                if (!StrUtil.equals(s, id)) {
-                    ReqUtil.writeResponse(response, R.fail().setMsg("非法的请求:用户会话与登录凭据不匹配"));
+                // 验证是否单点登录请求
+                if (!DEFAULT_ANT_PATH_REQUEST_MATCHER.matcher(request).isMatch()) {
+                    LoginUser loginUser = AuthUtil.getLoginUser();
+                    AppLoginUserInfo userInfo = (AppLoginUserInfo) loginUser.getUserInfo();
+                    String s = userInfo.getLoginAppId();
+                    if (!StrUtil.equals(s, id)) {
+                        ReqUtil.writeResponse(response, R.fail().setMsg("非法的请求:用户会话与登录凭据不匹配"));
+                        return false;
+                    }
                 }
             }
             return true;
